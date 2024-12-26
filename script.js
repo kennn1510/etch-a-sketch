97115cbed306ff43bb7b81dc0a64a240af09f455
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2024-12-26 20:53:51 +0700

    Add random colors and 16x16boxes

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const containerDiv = document.querySelector("#container");
-for (let i = 0; i < 16; i++) {
-  const gridDiv = document.createElement("div");
-  gridDiv.classList.add("grid");
-  containerDiv.appendChild(gridDiv);
+const gridContainer = document.querySelector("#grid-container");
+
+for (let i = 0; i < 16 * 16; i++) {
+  const gridItem = document.createElement("div");
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+  gridItem.style.flex = 1;
+  gridItem.style.backgroundColor = "#" + randomColor;
+
+  gridItem.classList.add("grid-item");
+
+  gridContainer.appendChild(gridItem);
 }
